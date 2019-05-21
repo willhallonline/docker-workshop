e52fa5a99777e16abe69a081c48c37731102b589
Will Hall <will.hall@cyanconnode.com>
2019-05-21 11:07:15 +0100

    Update index.js

--- workshop-2/index.js
+++ workshop-2/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
- res.send('I Love JS Containers! Especially with Drupal.');
+ res.send('I Love JS Containers!');
 });
 
 app.listen(3000, function () {
